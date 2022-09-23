7cb9aca2eff83c0d430147192c84b7d6c7e54d9b
Grace <grace941217@gmail.com>
2022-09-22 22:56:12 -0400

    commit tree 4abc55267a912c7589689097c027476d3abf12f4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM();
+});
+
+// function updateDOM() {
+//     document.getElementById("text") 
+//         .innerHTML = "This is really cool!";
+// }
+
+        
+function updateDOM() {
+    document.querySelector('p').textContent = "This is really cool!";
+}
